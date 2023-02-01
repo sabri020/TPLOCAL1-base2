76c040f57a3cb092f72000bc21a2c6acfe21e726
sabri020 <122880409+sabri020@users.noreply.github.com>
2023-02-01 15:32:41 +0100

    Validation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text.RegularExpressions;
 using TPLOCAL1.Models;
+using System.Reflection;
 
 //L'énoncé du tp et le logo hn sont livrés dans le répertoire /ressources de la solution
 //--------------------------------------------------------------------------------------
@@ -13,6 +15,7 @@ namespace TPLOCAL1.Controllers
 {
     public class HomeController : Controller
     {
+
         // méthode appelée par la routeur "naturellement"
         public ActionResult Index(string id)
         {
@@ -26,8 +29,11 @@ namespace TPLOCAL1.Controllers
                 {
                     case "ListeAvis":
                         //reste à faire : coder la lecture du fichier xml fourni
-                        return View(id);
+                        ListeAvis avisDonnes = new ListeAvis();
+
+                        return View(id, avisDonnes.GetAvis(Server.MapPath("~/FichierXML/DataAvis.xml"))) ;
                     case "Formulaire":
+                      
                         //reste à faire : appeler la vue Formulaire avec le modèle de données vide
                         return View(id);
                     default:
@@ -37,15 +43,32 @@ namespace TPLOCAL1.Controllers
             }
         }
 
-
+      
         //méthode pour envoyer les données du formulaire vers la page de validation
         [HttpPost]
-        public ActionResult ValidationFormulaire(/*model*/)
+        public ActionResult ValidationFormulaire(FormulaireModel modelv)
         {
+            
+            if (modelv.Sexe != "Homme" || modelv.Sexe != "Femme" || modelv.Sexe != "Autre")
+            {
+                ModelState.AddModelError("", "Merci de vérifier le Sexe"+ modelv.Sexe);
+            }
+            if (modelv.FormationASuivire == null)
+            {
+                ModelState.AddModelError("", "Merci de vérifier le Type de Formation");
+
+            }
+
+
+
+            if (!ModelState.IsValid)
+            {
+                return View("Formulaire");
+            }
             //reste à faire : tester de si les champs du modele sont bien remplis
             //s'ils ne sont pas bien remplis, afficher une erreur et rester sur la page formulaire
             //sinon, appeler la page ValidationFormulaire avec les données remplies par l'utilisateur
-                
+            return View(modelv);
         }
     }
 }
---
+++ Models/FormulaireModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace TPLOCAL1.Models
+{
+    public class FormulaireModel
+    {
+        [Display(Name = "Nom")]
+        [Required(ErrorMessage = "Merci de vérifier le Nom")]
+        public string Nom { get; set; }
+
+        [Display(Name = "Prénom")]
+        [Required(ErrorMessage = "Merci de vérifier le Prénom")]
+        public string Prenom { get; set; }
+
+        [Display(Name = "Sexe")]
+        [Required(ErrorMessage = "Merci de vérifier le Sexe")]
+        public string Sexe { get; set; }
+
+
+        [Display(Name = "Adresse")]
+        [Required(ErrorMessage = "Merci de vérifier l'Adresse")]
+        public string Adresse { get; set; }
+
+        [Display(Name = "Code Postal")]
+        [Required(ErrorMessage = "Merci de vérifier le Code Postal")]
+        [RegularExpression(@"^[0-9]{5}$", ErrorMessage = "Code Postal doit être sur 5 caractères numériques ")]
+        public string Cpostal { get; set; }
+
+        [Display(Name = "Ville")]
+        [Required(ErrorMessage = "Merci de vérifier la Ville")]
+        public string Ville { get; set; }
+
+        [Display(Name = "Adresse Mail")]
+        [Required(ErrorMessage = "L'adresse email n'est pas valide")]
+        [RegularExpression(@"^([\w]+)@([\w]+)\.([\w]+)$")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Date début formation")]
+        [Required(ErrorMessage = "La date doit être inférieure au  01 / 01 / 2021")]
+        [Range(typeof(DateTime), "1/1/2011", "1/1/2021")]
+        public string DateF { get; set; }
+
+        [Display(Name = "Formation Suivie")]
+        [Required(ErrorMessage = "Merci de vérifier le Type de Formation")]
+        public string FormationASuivire { get; set; }
+
+
+
+
+        [Display(Name = "Formation Cobol")]
+        public string Fcobol { get; set; }
+
+        [Display(Name = "Formation C#")]
+        public string Csharp { get; set; }
+    }
+}
